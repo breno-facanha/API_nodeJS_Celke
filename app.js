4ef7745fe67a65dad08cb2f69c9f21f9a288901c
Breno Façanha <brenofacanha.ti@gmail.com>
2024-10-22 00:54:42 -0300

    aprendendo api com nodejs, implantando rota get e post com req.body req.parms e req.query

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Listar usuários!");
+});
+
+app.get("/users/:id", (req, res) => {
+  let id = req.params.id;
+  let name = req.query.name
+  let email = req.query.email
+  return res.json({
+    id: id,
+    name: name,
+    email: email
+  })
+//   res.send(`Vizualizar id: ${id}`);
+});
+
+app.post("/users", (req, res) => {
+    let { name, email } = req.body
+   
+    return res.json({
+        name: name,
+        email: email
+      })
+})
+
+app.listen(8080, () => {
+  console.log("servidor rodando na porta 8080");
+});
